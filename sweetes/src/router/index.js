6435e1aac4d092ded58edae60965085d65799837
CamelliaEve <1551855824@qq.com>
2019-07-24 12:38:14 +0800

    破依赖毁我人生

--- sweetes/src/router/index.js
+++ sweetes/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import like from '../pages/like/like.vue'
 
 Vue.use(Router)
 
@@ -8,8 +8,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'like',
+      component: like
+    },
+    {
+      path:'/like',
+      name:'like',
+      component:like
     }
   ]
 })
