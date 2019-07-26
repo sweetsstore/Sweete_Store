bf7311ed5e50e9f133c8936b101e48ca5b991637
CamelliaEve <1551855824@qq.com>
2019-07-26 21:38:50 +0800

    评价组件雏形

--- sweetes/src/router/index.js
+++ sweetes/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import comment from '../Pages/comment/comment.vue'
+import degree from '../Pages/degree/degree.vue'
 
 Vue.use(Router)
 
@@ -15,6 +16,16 @@ export default new Router({
       path: '/comment',
       name: 'comment',
       component: comment
+    },
+    {
+      path: '/',
+      name: 'degree',
+      component: degree
+    },
+    {
+      path: '/degree',
+      name: 'degree',
+      component: degree
     }
   ]
 })
