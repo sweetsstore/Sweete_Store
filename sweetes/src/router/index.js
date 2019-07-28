88c7b389344fcdec5440ba967045d216e6fbb921
CamelliaEve <1551855824@qq.com>
2019-07-28 21:25:59 +0800

    添加种草详情页

--- sweetes/src/router/index.js
+++ sweetes/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import comment from '../Pages/comment/comment.vue'
 import merchandise from '../Pages/merchandise/merchandise.vue'
 import review from '../Pages/review/review.vue'
+import recommend from '../Pages/recommend/recommend.vue'
 
 Vue.use(Router)
 
@@ -37,6 +38,16 @@ export default new Router({
       path: '/review',
       name: 'review',
       component: review
+    },
+    {
+      path: '/',
+      name: 'recommend',
+      component: recommend
+    },
+    {
+      path: '/recommend',
+      name: 'recommend',
+      component: recommend
     }
   ]
 })
