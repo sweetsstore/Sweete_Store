8178b2fe582f0a39d4dc56e34d6dd59419dcfa15
xiaoxiaoyu2109281818 <2109281818@qq.com>
2019-07-31 10:02:29 +0800

    6push

--- sweetes/src/Pages/shoppingcart/shoppingcart.vue
+++ sweetes/src/Pages/shoppingcart/shoppingcart.vue
@@ -1,7 +1,21 @@
 <template>
     <div id="app">
         <div id="gouwuche">购物车</div>
-        <div id="body"></div>
+        <div id="body">
+            <div class="shopping">
+                <p id="shopname">纪梵希旗舰店</p>
+                <div>
+                    <div id="goodspic"></div>
+                    <div id="goodsname"></div>
+                    <span id="price"></span>
+                    <div>
+                        <input type="button" value="-" />
+                        <strong>0</strong>
+                        <input type="button" value="+" />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -10,8 +24,8 @@ export default {
 </script>
 
 <style scoped>
-    .app{
-        width: 100%;
+    *{
+        margin:0;
     }
     #gouwuche{
         width: 98%;
@@ -20,13 +34,20 @@ export default {
         top: 0;
         color: black;
         background-color: white;
-        font-size: 20px;
+        font-size: 18px;
         float: left;
         vertical-align: center;
-        margin: 1%;
+        padding: 1%;
     }
     #body{
         width: 100%;
         background: -webkit-linear-gradient(#ddd,#fff);
+        margin: 0;
+    }
+    .shopping{
+        width: 100%;
+        margin: 2% 0 2% 0;
+        height: 80px;
+        background-color: white;
     }
 </style>
